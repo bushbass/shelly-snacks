0e271db8c8a631b025bfaae8e327da101d311b00
Alex Nielsen <alex@bigal.org>
2020-08-23 11:41:06 -0400

    moved header view cart link to new component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import "./styles.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import About from "./components/About";
-import Products from "./components/Products";
-import Login from "./components/Login";
-import shoppingCartContext from "./shoppingCartContext";
-import ViewCart from "./components/ViewCart";
+import './styles.css';
+import Header from './components/Header';
+import HeaderCartLink from './components/HeaderCartLink';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Products from './components/Products';
+import Login from './components/Login';
+import shoppingCartContext from './shoppingCartContext';
+import ViewCart from './components/ViewCart';
 
 export default function App() {
-  const [currentUser, setCurrentUser] = useState("");
-  const [firstName, setFirstName] = useState("");
+  const [currentUser, setCurrentUser] = useState('');
+  const [firstName, setFirstName] = useState('');
   const [cart, setCart] = useState([]);
 
   const handleLoginChange = (event) => setFirstName(event.target.value);
@@ -46,7 +47,7 @@ export default function App() {
   return (
     <shoppingCartContext.Provider
       value={{
-        siteTitle: "Shelly Snacks!",
+        siteTitle: 'Shelly Snacks!',
         currentUser,
         firstName,
         cart,
@@ -56,47 +57,29 @@ export default function App() {
         addToCart,
         emptyCart,
         decrementCartItem,
-        incrementCartItem
+        incrementCartItem,
       }}
     >
       <Router>
-        <div className="App">
-          <div className="header-cart">
-            <Link to="/cart">
-              <span>{cart.length}</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-shopping-cart"
-              >
-                <circle cx="9" cy="21" r="1" />
-                <circle cx="20" cy="21" r="1" />
-                <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
-              </svg>
-            </Link>
-          </div>
+        <div className='App'>
+          <HeaderCartLink />
           <Header />
 
           <main>
             <Switch>
-              <Route path="/login">
+              <Route path='/login'>
                 <Login />
               </Route>
-              <Route path="/about">
+              <Route path='/about'>
                 <About />
               </Route>
-              <Route path="/products">
+              <Route path='/products'>
                 <Products />
               </Route>
-              <Route path="/cart">
+              <Route path='/cart'>
                 <ViewCart />
               </Route>
-              <Route path="/">
+              <Route path='/'>
                 <Home />
               </Route>
             </Switch>
